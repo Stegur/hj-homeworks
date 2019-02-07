4535d3e271986f4a345704949ebfe3298b7440ed
Alex Stegura <stegura.alex@gmail.com>
2019-02-07 11:52:51 +0300

    alpha

--- dom/modal-window/js/ui.js
+++ dom/modal-window/js/ui.js
@@ -1,2 +1,16 @@
 // Инициализация функционала модального окна
-initModalUi();
+
+
+//document.addEventListener('DOMContentLoaded', go); // из-за чего получилась задержка?
+
+// setTimeout(go, 3000);
+// function go() {
+//     const btns = document.querySelectorAll('.trigger');
+//     console.log(btns)
+//     btnShow.addEventListener('click', initModalUi);
+// }
+
+
+const btn = document.getElementsByClassName('btn-close')[0];
+console.log(btn)
+btn.addEventListener('click', initModalUi);
