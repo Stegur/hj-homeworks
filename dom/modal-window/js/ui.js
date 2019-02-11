da96302e4ef459eb7166564fce024788630654fc
Stegura Alex <stegura.alex@gmail.com>
2019-02-11 22:24:11 +0300

    almost final

--- dom/modal-window/js/ui.js
+++ dom/modal-window/js/ui.js
@@ -1,4 +1,14 @@
 // Инициализация функционала модального окна
+// initModalUi();
+
+document.addEventListener('DOMContentLoaded', initModalUi);
+
+const btn = document.querySelector('.btn'); // null?
+console.log(btn);
+
+const anotherBtn = document.getElementsByClassName('btn')[0]; // undefined?
+console.log(anotherBtn);
+
 
 
 //document.addEventListener('DOMContentLoaded', go); // из-за чего получилась задержка?
@@ -11,6 +21,6 @@
 // }
 
 
-const btn = document.getElementsByClassName('btn-close')[0];
-console.log(btn)
-btn.addEventListener('click', initModalUi);
+// const btn = document.getElementsByClassName('btn-close')[0];
+// console.log(btn)
+// btn.addEventListener('click', initModalUi);
