f339a1fa5a0ff46a1e70f37ed1a2362a681c7c46
Stegura Alex <stegura.alex@gmail.com>
2019-02-18 23:03:19 +0300

    alpha

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const itemsList = document.getElementsByClassName('items-list')[0];
+const more = document.querySelector('.show-more');
+
+document.addEventListener('DOMContentLoaded', some);
+
+function some() {
+    for (let item of itemsList.children) {
+        let btn = item.querySelector('.add-to-cart');
+        btn.addEventListener('click', prepareForCart);
+    }
+
+}
+
+function prepareForCart(event) {
+
+    const title = event.target.dataset.title;
+    const price  = event.target.dataset.price;
+
+    addToCart({title, price});
+
+}
+
+more.addEventListener('click', update);
+
+function update() {
+    some();
+}
+
+
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    const table = document.querySelector('table');
+    const tr = event.target;
+
+    if (tr.dataset.dir === undefined) {
+        tr.dataset.dir = '1';
+    } else if (tr.dataset.dir === '1') {
+        tr.dataset.dir = '-1';
+    } else {
+        tr.dataset.dir = '1';
+    }
+
+    table.dataset.sortBy = tr.dataset.propName;
+
+    sortTable(tr.dataset.propName, tr.dataset.dir);
 }
